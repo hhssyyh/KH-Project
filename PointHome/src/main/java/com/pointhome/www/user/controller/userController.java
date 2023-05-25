d21654d83433edae65c3dfdb741af6bf24149e38
HaeSeong Kim <106731475+harrysonkim@users.noreply.github.com>
2023-05-25 21:33:24 +0900

    230525 김해성 소셜로그인(네이버) 구현중

--- PointHome/src/main/java/com/pointhome/www/user/controller/userController.java
+++ PointHome/src/main/java/com/pointhome/www/user/controller/userController.java
@@ -1,5 +1,12 @@
 package com.pointhome.www.user.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -8,7 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.pointhome.www.user.dto.User;
 import com.pointhome.www.user.service.face.UserService;
 
@@ -46,7 +56,6 @@ public class userController {
 		
 		Boolean login = userService.isLogin(Param);
 		
-		
 		if (login) {
 			
 			User user = new User();
@@ -72,6 +81,168 @@ public class userController {
 		
 		return "redirect:/";
 	}
+
+	@GetMapping("/user/searchid")
+	public void searchid() {
+		logger.debug("/user/searchid [GET]");
+		
+	}
+
+	@PostMapping("/user/searchid")
+	public void searchidProc() {
+		logger.debug("/user/searchid [POST]");
+		
+	}
+
+	@GetMapping("/user/searchpw")
+	public void searchpw() {
+		logger.debug("/user/searchpw [GET]");
+		
+	}
+
+	@PostMapping("/user/searchpw")
+	public void searchpwProc() {
+		logger.debug("/user/searchpw [POST]");
+		
+	}
+
+	@GetMapping("/user/naverlogin")
+	public ModelAndView naverlogin(HttpSession session) {
+		logger.debug("/user/naverlogin [GET]");
+
+		Map<String, Object> map = userService.getStateApiUrl();
+				
+	    String apiURL = (String) map.get("apiURL");
+	    String state = (String) map.get("state");
+	    logger.debug("{}",apiURL);
+	    
+	    session.setAttribute("state", state);
+	    
+	    // 외부 URL로 retrun
+	    return new ModelAndView("redirect:" + apiURL);
+	}
+
+	@GetMapping("/user/callback")
+	public String naverCallback(HttpServletRequest request, HttpSession session) {
+		logger.debug("/user/callback [GET]");
+		
+		String code = request.getParameter("code");
+		String state = (String)session.getAttribute("state");
+
+		String apiURL = userService.getTokenNaver(code, state);
+		
+		String accessToken = "";
+		String refreshToken = "";
+
+		try {
+			URL url = new URL(apiURL);
+			
+			HttpURLConnection con = (HttpURLConnection)url.openConnection();
+			
+			con.setRequestMethod("GET");
+			
+			int responseCode = con.getResponseCode();
+			
+			BufferedReader br;
+			
+			if (responseCode == 200) { // 정상 호출
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {  // 에러 발생
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+
+			String inputLine;
+			String res = "";;
+
+			while ((inputLine = br.readLine()) != null) {
+				res += inputLine;
+			}
+
+			br.close();
+
+			if (responseCode == 200) {
+				logger.debug(res.toString());
+
+				Gson gson = new Gson();
+
+				JsonObject jsonObj = gson.fromJson(res, JsonObject.class);
+				logger.debug("{}", jsonObj);
+
+				accessToken = jsonObj.get("access_token").getAsString();
+				refreshToken = jsonObj.get("refresh_token").getAsString();
+				logger.debug("{}", accessToken);
+				logger.debug("{}", refreshToken);
+
+			}
+
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+////////////////////////////////////////////////////////////////
+
+		if(accessToken != null) { // access_token을 잘 받아왔다면
+
+			try {
+
+				String apiurl = "https://openapi.naver.com/v1/nid/me";
+				URL url = new URL(apiurl);
+				HttpURLConnection con = (HttpURLConnection)url.openConnection();
+				con.setRequestMethod("GET");
+//				con.setRequestProperty("Authorization", header);
+				int responseCode = con.getResponseCode();
+				BufferedReader br;
+
+				if(responseCode==200) { // 정상 호출
+				  br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				} else {  // 에러 발생
+				  br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+				}
+
+				String inputLine;
+				StringBuffer res = new StringBuffer();
+				while ((inputLine = br.readLine()) != null) {
+				  res.append(inputLine);
+				}
+
+				br.close();
+
+//				Gson gson = new Gson();
+//				Object obj = gson.fromJson(res, JsonObject.class);
+//				JSONObject jsonObj = (JSONObject)obj;
+//				JSONObject resObj = (JSONObject)jsonObj.get("response");
+//				 
+				//왼쪽 변수 이름은 원하는 대로 정하면 된다. 
+				//단, 우측의 get()안에 들어가는 값은 와인색 상자 안의 값을 그대로 적어주어야 한다.
+//				String naverCode = (String)resObj.get("id");
+//				String email = (String)resObj.get("email");
+//				String name = (String)resObj.get("name");
+//				String nickName = (String)resObj.get("nickname");
+				
+		    } catch (Exception e) {
+
+		    	e.printStackTrace();
+
+		    }
+
+		}
+		
+		
+		
+		  return "redirect:/";
+		  
+	}
+	
+	
+	
+	@GetMapping("/user/kakaologin")
+	public void kakaoLogin() {
+		logger.debug("/user/kakaoLogin [GET]");
+		
+	}
+	
 
 
 }
--- PointHome/src/main/java/com/pointhome/www/user/service/impl/UserServiceImpl.java
+++ PointHome/src/main/java/com/pointhome/www/user/service/impl/UserServiceImpl.java
@@ -1,5 +1,12 @@
 package com.pointhome.www.user.service.impl;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,6 +55,68 @@ public class UserServiceImpl implements UserService {
 
 		
 		return userDao.selectByUserId(userno);
+	}
+
+	@Override
+	public String generateState() {
+
+	    SecureRandom random = new SecureRandom();
+	    return new BigInteger(130, random).toString(32);
+	}
+
+	@Override
+	public Map<String, Object> getStateApiUrl() {
+
+		Map<String, Object> map = new HashMap<>();
+		
+		String callbackURL = "http://localhost:8888/user/callback";
+	    String clientId = "cUmMzSZPUu5v9OklWSqJ";//애플리케이션 클라이언트 아이디값";
+	    
+	    String redirectURI = null;
+		try {
+			redirectURI = URLEncoder.encode(callbackURL, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	    
+		SecureRandom random = new SecureRandom();
+	    String state = new BigInteger(130, random).toString(32);
+	    String apiURL = "";
+	    apiURL += "https://nid.naver.com/oauth2.0/authorize?response_type=code";
+        apiURL += "&client_id=" + clientId;
+        apiURL += "&redirect_uri=" + redirectURI;
+        apiURL += "&state=" + state;
+		
+        map.put("state", state);
+        map.put("apiURL", apiURL);
+		
+        logger.debug("{}", map.put("apiURL", apiURL));
+		return map;
+	}
+
+	@Override
+	public String getTokenNaver(String code, String state) {
+
+		String callbackURL = "http://localhost:8888/";
+		String clientId = "cUmMzSZPUu5v9OklWSqJ";//애플리케이션 클라이언트 아이디값";
+		String clientSecret = "7us20334CP";//애플리케이션 클라이언트 시크릿값";
+		String redirectURI = null;
+
+		try {
+			redirectURI = URLEncoder.encode(callbackURL, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		String apiURL = "";
+		apiURL += "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code";
+		apiURL += "&client_id=" + clientId;
+		apiURL += "&client_secret=" + clientSecret;
+		apiURL += "&redirect_uri=" + redirectURI;
+		apiURL += "&code=" + code;
+		apiURL += "&state=" + state;
+		
+		return apiURL;
 	}
 
 
